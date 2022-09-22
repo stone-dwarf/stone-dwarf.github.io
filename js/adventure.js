0b833d3ac423631712cd7d5075c0acf5de93bbd0
grant freeman <grantfreeman@pm.me>
2022-09-21 22:01:22 -0500

    implement creature stat blocks

--- js/adventure.js
+++ js/adventure.js
@@ -1,5 +1,341 @@
 import $ from '/js/jquery.js'
 
+class CreatureStatBlock {
+  #template = `
+  <h5 class="creature-quantity"></h5>
+  <h3 class="creature-name"></h3>
+  <div class="creature-descriptor"></div>
+  <div class="rank-frame">
+    <span class="rank-header">Rank</span>
+    <span class="rank-value"></span>
+    <input type="range" class="creature-rank u-full-width">
+  </div>
+  <div class="major-stat-headers row">
+    <div class="four columns maj-stat-hdr">HP</div>
+    <div class="four columns maj-stat-hdr">AC</div>
+    <div class="four columns maj-stat-hdr">SP</div>
+  </div>
+  <div class="major-stats row">
+    <div class="four columns maj-stat hit-point-text"></div>
+    <div class="four columns maj-stat armor-class-text"></div>
+    <div class="four columns maj-stat speed-text"></div>
+  </div>
+  <table class="modifiers u-full-width">
+    <thead>
+      <tr>
+        <th class="ability-header">STR</th>
+        <th class="ability-header">DEX</th>
+        <th class="ability-header">CON</th>
+        <th class="ability-header">INT</th>
+        <th class="ability-header">WIS</th>
+        <th class="ability-header">CHA</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td class="str-mod ability-mod"></td>
+        <td class="dex-mod ability-mod"></td>
+        <td class="con-mod ability-mod"></td>
+        <td class="int-mod ability-mod"></td>
+        <td class="wis-mod ability-mod"></td>
+        <td class="cha-mod ability-mod"></td>
+      </tr>
+    </tbody>
+  </table>
+  <div class="traits-header">Traits</div>
+  <div class="traits"></div>
+  <div class="behavior"></div>
+  <div class="passives-header">Passives</div>
+  <div class="passives"></div>
+  <div class="actions-header">Actions</div>
+  <div class="actions"></div>
+  <div class="bonus-actions-header">Bonus Actions</div>
+  <div class="bonus-actions"></div>
+  <div class="reactions-header">Reactions</div>
+  <div class="reactions"></div>
+  `
+
+  /**
+   * creates a creature stat block
+   * @param {PartySizeSlider} partyCls party size object
+   * @param {String} frameId id of parent dom to append content to
+   */
+  constructor (partyCls, frameId) {
+    const self = this
+    this.party = partyCls
+    this.frame = $('#' + frameId)
+    this.rank = 0 // default
+    this.genQuantity = (s) => { return 0 } // default
+
+    $(this.frame).html(this.#template)
+
+    this.party.subscribe(this, this.onNewPartySize)
+
+    $(this.frame).find('.creature-rank')[0].oninput = function () {
+      self.onNewRank(this.value)
+    }
+  }
+
+  setAbilityModifiers (abilityModList) {
+    this.abilityModList = abilityModList
+  }
+
+  setActions (actions) {
+    const frame = $(this.frame).find('.actions')[0]
+    $(frame).empty()
+
+    actions.forEach((a) => {
+      const aStr = `<div class="action" rank="${a.rank}"><span class="action-name">${a.name}</span>${a.desc}</div>`
+      $(frame).append(aStr)
+    })
+  }
+
+  setArmorClass (acList) {
+    this.acList = acList
+  }
+
+  setBehavior (behavior) {
+    $(this.frame).find('.behavior').html(behavior)
+  }
+
+  setBonusActions (bonusActions) {
+    const frame = $(this.frame).find('.bonus-actions')[0]
+    $(frame).empty()
+
+    bonusActions.forEach((b) => {
+      const bStr = `<div class="bonus-action" rank="${b.rank}"><span class="bonus-action-name">${b.name}</span>${b.desc}</div>`
+      $(frame).append(bStr)
+    })
+  }
+
+  setDescriptor (text) {
+    $(this.frame).find('.creature-descriptor').text(text)
+  }
+
+  setHitPoints (hpList) {
+    this.hpList = hpList
+  }
+
+  setName (text) {
+    $(this.frame).find('.creature-name').text(text)
+  }
+
+  setPassives (passives) {
+    const frame = $(this.frame).find('.passives')[0]
+    $(frame).empty()
+
+    passives.forEach((p) => {
+      const pStr = `<div class="passive" rank="${p.rank}"><span class="passive-name">${p.name}</span>${p.desc}</div>`
+      $(frame).append(pStr)
+    })
+  }
+
+  setQuantityFunction (func) {
+    this.genQuantity = func
+  }
+
+  setRanks (numOfRanks) {
+    this.min = 0
+    this.max = numOfRanks - 1
+    this.rank = this.min
+
+    $(this.frame).find('.creature-rank').attr({
+      min: this.min,
+      max: this.max,
+      value: this.rank
+    })
+  }
+
+  setReactions (reactions) {
+    const frame = $(this.frame).find('.reactions')[0]
+    $(frame).empty()
+
+    reactions.forEach((r) => {
+      const rStr = `<div class="reaction" rank="${r.rank}"><span class="reaction-name">${r.name}</span>${r.desc}</div>`
+      $(frame).append(rStr)
+    })
+  }
+
+  setSpeed (spList) {
+    this.spList = spList
+  }
+
+  setTraits (traits) {
+    const frame = $(this.frame).find('.traits')[0]
+    $(frame).empty()
+
+    for (const t in traits) {
+      const tStr = `<span class="trait-name">${t}</span>${traits[t]}`
+      $(frame).append(tStr)
+    }
+  }
+
+  refresh () {
+    this.onNewPartySize()
+    this.onNewRank()
+  }
+
+  onNewPartySize (size = null) {
+    size ??= this.party.size
+    const qty = this.genQuantity(size)
+    $(this.frame).find('.creature-quantity').html(qty + 'x')
+    this.updateHitPoints()
+  }
+
+  onNewRank (rank = null) {
+    this.rank = rank ?? this.rank
+    this.updateAbilityModifiers()
+    this.updateArmorClass()
+    this.updateHitPoints()
+    this.updateRankDisplay()
+    this.updateSpeed()
+    this.filterActionAbilities()
+  }
+
+  updateAbilityModifiers () {
+    const modOrder = ['str-mod', 'dex-mod', 'con-mod', 'int-mod', 'wis-mod', 'cha-mod']
+    const modList = this.abilityModList[this.rank]
+    for (let i = 0; i < modOrder.length; ++i) {
+      let mod = modList[i]
+      if (mod > 0) {
+        mod = `+${mod}`
+      }
+      $(this.frame).find('.' + modOrder[i]).html(mod)
+    }
+  }
+
+  updateArmorClass () {
+    const acValue = this.acList[this.rank].ac
+    const acType = this.acList[this.rank].type
+    let acStr = null
+
+    if (acType === null) {
+      acStr = `<span class="major-stats-big">${acValue}</span>`
+    } else {
+      acStr = `<span class="major-stats-big">${acValue}</span><br>(${acType})`
+    }
+
+    $(this.frame).find('.armor-class-text').html(acStr)
+  }
+
+  updateHitPoints () {
+    if (this.hpList === undefined) { return }
+    const hp = this.hpList[this.rank]
+    const avg = hp.base + ((hp.sides / 2) * this.party.size)
+    $(this.frame).find('.hit-point-text').html(`
+      <span class="major-stats-big">${avg}</span>
+      <br>
+      (${this.party.size}d${hp.sides} + ${hp.base})`)
+  }
+
+  updateRankDisplay() {
+    $(this.frame).find('.rank-value').html(parseInt(this.rank) + 1)
+  }
+
+  updateSpeed () {
+    const speed = this.spList[this.rank].sp
+    const spType = this.spList[this.rank].type
+    let spStr = null
+
+    if (spType === null) {
+      spStr = `<span class="major-stats-big">${speed}</span>`
+    } else {
+      spStr = `<span class="major-stats-big">${speed}</span><br>(${spType})`
+    }
+    $(this.frame).find('.speed-text').html(spStr)
+  }
+
+  filterActionAbilities () {
+    let active = 0
+
+    // passive abilities
+    active = 0
+    const pHead = $(this.frame).find('.passives-header')
+    const pList = $(this.frame).find('.passive')
+    if (pList.length > 0) {
+      $(pList).each((i, passive) => {
+        if (parseInt($(passive).attr('rank')) <= this.rank) {
+          $(passive).show()
+          active++
+        } else {
+          $(passive).hide()
+        }
+      })
+    }
+    active > 0 ? $(pHead).show() : $(pHead).hide()
+
+    // action abilities
+    active = 0
+    const aHead = $(this.frame).find('.actions-header')
+    const aList = $(this.frame).find('.action')
+    if (aList.length > 0) {
+      $(aList).each((i, action) => {
+        if (parseInt($(action).attr('rank')) <= this.rank) {
+          $(action).show()
+          active++
+        } else {
+          $(action).hide()
+        }
+      })
+    }
+    active > 0 ? $(aHead).show() : $(aHead).hide()
+
+    // bonus action abilities
+    active = 0
+    const bHead = $(this.frame).find('.bonus-actions-header')
+    const bList = $(this.frame).find('.bonus-action')
+    if (bList.length > 0) {
+      $(bList).each((i, bonusAction) => {
+        if (parseInt($(bonusAction).attr('rank')) <= this.rank) {
+          $(bonusAction).show()
+          active++
+        } else {
+          $(bonusAction).hide()
+        }
+      })
+    }
+    active > 0 ? $(bHead).show() : $(bHead).hide()
+
+    // reaction abilities
+    active = 0
+    const rHead = $(this.frame).find('.reactions-header')
+    const rList = $(this.frame).find('.reaction')
+    if (rList.length > 0) {
+      $(rList).each((i, reaction) => {
+        if (parseInt($(reaction).attr('rank')) <= this.rank) {
+          $(reaction).show()
+          active++
+        } else {
+          $(reaction).hide()
+        }
+      })
+    }
+    active > 0 ? $(rHead).show() : $(rHead).hide()
+  }
+}
+
+class DynamicText {
+  /**
+   * provide necessary objects to provide a dynamic value on the page
+   * @function generator
+   * @param {PartySizeSlider} partyCls object of current slider
+   * @param {String} frameId id of parent dom to insert text
+   * @param {generator(Number)} genText generate text based on provided party size
+   */
+  constructor (partyCls, frameId, genText) {
+    this.party = partyCls
+    this.frame = $('#' + frameId)
+    this.text = genText
+
+    // sub to party size changes
+    this.party.subscribe(this, this.onNewPartySize)
+  }
+
+  onNewPartySize (size) {
+    $(this.frame).html(this.text(size))
+  }
+}
+
 class PartySizeSlider {
   // private fields
   #value
@@ -72,26 +408,4 @@ class PartySizeSlider {
   }
 }
 
-class DynamicText {
-  /**
-   * provide necessary objects to provide a dynamic value on the page
-   * @function generator
-   * @param {PartySizeSlider} partyCls object of current slider
-   * @param {String} frameId id of parent dom to insert text
-   * @param {generator(Number)} genText generate text based on provided party size
-   */
-  constructor (partyCls, frameId, genText) {
-    this.party = partyCls
-    this.frame = $('#' + frameId)
-    this.text = genText
-
-    // sub to party size changes
-    this.party.subscribe(this, this.onNewPartySize)
-  }
-
-  onNewPartySize (size) {
-    $(this.frame).html(this.text(size))
-  }
-}
-
-export { DynamicText, PartySizeSlider }
+export { CreatureStatBlock, DynamicText, PartySizeSlider }
--- page/teethed-to-death.js
+++ page/teethed-to-death.js
@@ -1,6 +1,112 @@
-import { DynamicText, PartySizeSlider } from "/js/adventure.js"
+import { CreatureStatBlock, DynamicText, PartySizeSlider } from "/js/adventure.js"
 
 const party = new PartySizeSlider('party-size-selector')
 const donyaLoot = new DynamicText(party, 'donya-loot', (size) => {
   return (size * 5) + ' gp'
 })
+
+const oleg = new CreatureStatBlock(party, 'oleg')
+oleg.setQuantityFunction(function (size) { return 1 })
+oleg.setName('Oleg')
+oleg.setDescriptor('medium humanoid (human), neutral good')
+oleg.setRanks(4)
+oleg.setHitPoints([
+  { base: 5, sides: 4 },
+  { base: 10, sides: 4 },
+  { base: 18, sides: 4 },
+  { base: 30, sides: 4 }])
+oleg.setArmorClass([
+  { ac: 10, type: null },
+  { ac: 11, type: null },
+  { ac: 11, type: null },
+  { ac: 12, type: null }])
+oleg.setSpeed([
+  { sp: 30, type: null },
+  { sp: 30, type: null },
+  { sp: 30, type: null },
+  { sp: 30, type: null }])
+oleg.setAbilityModifiers([
+  [0, 0, 0, 0, 1, 0],
+  [0, 1, 0, 0, 1, 0],
+  [0, 1, 0, 0, 1, 0],
+  [0, 2, 0, 0, 1, 0]])
+oleg.setTraits({ Languages: 'Common' })
+oleg.setBehavior('Oleg will try to stop the players from hurting Kobal. He\'ll prioritize grappling players before resorting to punching if that fails or is attacked. If Kobal goes unconcious, Oleg will stop fighting and try to aid his son.')
+oleg.setActions([
+  {
+    rank: 0,
+    name: 'Grapple',
+    desc: 'Attempts to <em>grapple</em> any player that attacks Kobal.'
+  },
+  {
+    rank: 0,
+    name: 'Jab',
+    desc: '+2 to hit. 1d4 bludgeoning damage.'
+  },
+  {
+    rank: 3,
+    name: 'Cross',
+    desc: '+3 to hit. 1d6 bludgeoning damage.'
+  }])
+oleg.setBonusActions([
+  {
+    rank: 2,
+    name: 'Uppercut',
+    desc: '+2 to hit. 1d4 points of bludgeoning damage.'
+  }])
+oleg.setReactions([
+  {
+    rank: 1,
+    name: 'Left Hook',
+    desc: '+2 to hit. 1d4 points of bludgeoning damage.'
+  }])
+oleg.refresh()
+
+const kobal = new CreatureStatBlock(party, 'kobal')
+kobal.setQuantityFunction(function (size) { return 1 })
+kobal.setName('Kobal')
+kobal.setDescriptor('medium humanoid (Strigoi), neutral evil')
+kobal.setRanks(3)
+kobal.setHitPoints([
+  { base: 3, sides: 4 },
+  { base: 6, sides: 4 },
+  { base: 10, sides: 4 }])
+kobal.setArmorClass([
+  { ac: 8, type: null },
+  { ac: 9, type: null },
+  { ac: 10, type: null }])
+kobal.setSpeed([
+  { sp: 30, type: null },
+  { sp: 30, type: null },
+  { sp: 30, type: null }])
+kobal.setAbilityModifiers([
+  [0, 1, 0, 0, 0, 0],
+  [1, 1, 0, 0, 0, 0],
+  [1, 2, 0, 0, 0, 0]])
+kobal.setTraits({ Languages: 'Common' })
+kobal.setBehavior('Kobal will always avoid attacking his father. No verbal persuasion will stop him from attacking.')
+kobal.setPassives([
+  {
+    rank: 0,
+    name: 'Agile',
+    desc: 'Lose no speed standing from prone.'
+  }])
+kobal.setActions([
+  {
+    rank: 0,
+    name: 'Bite',
+    desc: '+2 to hit. 1d4 piercing damage. (still unfanged)'
+  }])
+kobal.setBonusActions([
+  {
+    rank: 2,
+    name: 'Tackle',
+    desc: 'Attempt to tackle the target to the ground (supine). +1 to Strength contested with targets Strength or Dexterity.'
+  }])
+kobal.setReactions([
+  {
+    rank: 1,
+    name: 'Dodge',
+    desc: 'the next incoming attack has disadvantage if you can see the attacker.'
+  }])
+kobal.refresh()
