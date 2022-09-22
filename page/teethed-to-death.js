df9294015cfcd36a72a8afd5eecbe0c45b947053
grant freeman <grantfreeman@pm.me>
2022-09-21 23:02:25 -0500

    add color touchups

--- page/teethed-to-death.js
+++ page/teethed-to-death.js
@@ -110,3 +110,103 @@ kobal.setReactions([
     desc: 'the next incoming attack has disadvantage if you can see the attacker.'
   }])
 kobal.refresh()
+
+const servant = new CreatureStatBlock(party, 'strigoi-servant')
+servant.setQuantityFunction(function (partySize) { return Math.min(3, partySize - 1) })
+servant.setName('Strigoi Servant')
+servant.setDescriptor('medium humanoid (Strigoi), lawful evil')
+servant.setRanks(4)
+servant.setHitPoints([
+  { base: 2, sides: 2 },
+  { base: 2, sides: 4 },
+  { base: 8, sides: 4 },
+  { base: 16, sides: 4 }])
+servant.setArmorClass([
+  { ac: 8, type: null },
+  { ac: 9, type: null },
+  { ac: 10, type: null },
+  { ac: 10, type: null }])
+servant.setSpeed([
+  { sp: 30, type: null },
+  { sp: 30, type: null },
+  { sp: 30, type: null },
+  { sp: 30, type: null }])
+servant.setAbilityModifiers([
+  [0, 1, 0, 0, 0, 0],
+  [0, 1, 1, 0, 0, 0],
+  [0, 2, 1, 0, 0, 0],
+  [1, 2, 1, 0, 0, 0]])
+servant.setTraits({ Languages: 'Common' })
+servant.setBehavior('Strigoi Servants will spend first turn attacking the nearest party member. Then move to protect Cassandra.')
+servant.setPassives([
+  {
+    rank: 0,
+    name: 'Agile',
+    desc: 'Lose no speed standing from prone.'
+  }])
+servant.setActions([
+  {
+    rank: 0,
+    name: 'Scratch',
+    desc: '+1 to hit. 1d4 slashing damage.'
+  },
+  {
+    rank: 2,
+    name: 'Bite',
+    desc: '+2 to hit. 1d6 piercing damage.'
+  }])
+servant.setReactions([
+  {
+    rank: 1,
+    name: 'Kick',
+    desc: 'After taking melee damage, you can kick the attacker back 5 feet. Dexterity contested with attackers Strength of Dexterity.'
+  }])
+servant.refresh()
+
+const cassandra = new CreatureStatBlock(party, 'cassandra')
+cassandra.setQuantityFunction(function (partySize) { return 1 })
+cassandra.setName('Cassandra')
+cassandra.setDescriptor('medium humanoid (Strigoi), neutral')
+cassandra.setRanks(3)
+cassandra.setHitPoints([
+  { base: 1, sides: 4 },
+  { base: 4, sides: 4 },
+  { base: 8, sides: 4 }])
+cassandra.setArmorClass([
+  { ac: 8, type: null },
+  { ac: 9, type: null },
+  { ac: 10, type: null }])
+cassandra.setSpeed([
+  { sp: 30, type: null },
+  { sp: 30, type: null },
+  { sp: 30, type: null }])
+cassandra.setAbilityModifiers([
+  [0, 1, 0, 0, 0, 0],
+  [0, 1, 1, 0, 0, 0],
+  [0, 2, 1, 0, 0, 0]])
+cassandra.setTraits({ Languages: 'Common' })
+cassandra.setBehavior('When a player gets within 30-feet of Cassandra, she\'ll break through the woodent chest, with her hand, and use the Indica egg.<br><br>As Cassandra nears 0 hit points, streams of tears will begin to show out of fear.')
+cassandra.setPassives([
+  {
+    rank: 0,
+    name: 'Agile',
+    desc: 'Lose no speed standing from prone.'
+  }])
+cassandra.setActions([
+  {
+    rank: 0,
+    name: 'Scratch',
+    desc: '+1 to hit. 1d4 slashing damage.'
+  },
+  {
+    rank: 2,
+    name: 'Bite',
+    desc: '+2 to hit. 1d6 piercing damage.'
+  }])
+cassandra.setReactions([
+  {
+    rank: 1,
+    name: 'Kick',
+    desc: 'After taking melee damage, you can kick the attacker back 5 feet. Dexterity contested with attackers Strength of Dexterity.'
+  }])
+cassandra.refresh()
