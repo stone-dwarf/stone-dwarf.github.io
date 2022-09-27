a05b3a6506b2b4b9394ee31426f368e23275f806
grantfreeman <71525525+grantfreeman@users.noreply.github.com>
2022-09-27 11:39:47 -0500

    add weapons

--- js/merchant.js
+++ js/merchant.js
@@ -155,7 +155,339 @@ class ItemCompendium {
     /***************/
     /*** WEAPONS ***/
     /***************/
-
+    {
+      name: 'Battleaxe',
+      type: ItemCompendium.Filter.MARTIAL_MELEE_WEAPON,
+      cost: '10 gp',
+      damage: '1d8 Slashing',
+      weight: '4 lb.',
+      properties: 'Versatile (1d10)',
+      magical: false
+    },
+    {
+      name: 'Blowgun',
+      type: ItemCompendium.Filter.MARTIAL_RANGED_WEAPON,
+      cost: '10 gp',
+      damage: '1 Piercing',
+      weight: '1 lb.',
+      properties: 'Ammunition (range 25/100), Loading',
+      magical: false
+    },
+    {
+      name: 'Club',
+      type: ItemCompendium.Filter.SIMPLE_MELEE_WEAPON,
+      cost: '1 sp',
+      damage: '1d4 bludgeoning',
+      weight: '2 lb.',
+      properties: 'Light',
+      magical: false
+    },
+    {
+      name: 'Dagger',
+      type: ItemCompendium.Filter.SIMPLE_MELEE_WEAPON,
+      cost: '2 gp',
+      damage: '1d4 piercing',
+      weight: '1 lb.',
+      properties: 'Finesse, Light, Thrown (range 20/60)',
+      magical: false
+    },
+    {
+      name: 'Dart',
+      type: ItemCompendium.Filter.SIMPLE_RANGED_WEAPON,
+      cost: '5 cp',
+      damage: '1d4 Piercing',
+      weight: '1/4 lb.',
+      properties: 'Finesse, Thrown (range 20/60)',
+      magical: false
+    },
+    {
+      name: 'Flail',
+      type: ItemCompendium.Filter.MARTIAL_MELEE_WEAPON,
+      cost: '10 gp',
+      damage: '1d8 Bludgeoning',
+      weight: '4 lb.',
+      properties: null,
+      magical: false
+    },
+    {
+      name: 'Glaive',
+      type: ItemCompendium.Filter.MARTIAL_MELEE_WEAPON,
+      cost: '20 gp',
+      damage: '1d10 Slashing',
+      weight: '6 lb.',
+      properties: 'Heavy, Reach, Two-Handed',
+      magical: false
+    },
+    {
+      name: 'Greataxe',
+      type: ItemCompendium.Filter.MARTIAL_MELEE_WEAPON,
+      cost: '30 gp',
+      damage: '1d12 Slashing',
+      weight: '7 lb.',
+      properties: 'Heavy, Two-Handed',
+      magical: false
+    },
+    {
+      name: 'Greatclub',
+      type: ItemCompendium.Filter.SIMPLE_MELEE_WEAPON,
+      cost: '2 sp',
+      damage: '1d8 Bludgeoning',
+      weight: '10 lb.',
+      properties: 'Two-Handed',
+      magical: false
+    },
+    {
+      name: 'Greatsword',
+      type: ItemCompendium.Filter.MARTIAL_MELEE_WEAPON,
+      cost: '50 gp',
+      damage: '2d6 Slashing',
+      weight: '6 lb.',
+      properties: 'Heavy, Two-Handed',
+      magical: false
+    },
+    {
+      name: 'Halberd',
+      type: ItemCompendium.Filter.MARTIAL_MELEE_WEAPON,
+      cost: '20 gp',
+      damage: '1d10 Slashing',
+      weight: '6 lb.',
+      properties: 'Heavy, Reach, Two-Handed',
+      magical: false
+    },
+    {
+      name: 'Handaxe',
+      type: ItemCompendium.Filter.SIMPLE_MELEE_WEAPON,
+      cost: '5 gp',
+      damage: '1d6 Slashing',
+      weight: '2 lb.',
+      properties: 'Light, Thrown (range 20/60)',
+      magical: false
+    },
+    {
+      name: 'Hand Crossbow',
+      type: ItemCompendium.Filter.MARTIAL_RANGED_WEAPON,
+      cost: '75 gp',
+      damage: '1d6 Piercing',
+      weight: '3 lb.',
+      properties: 'Ammunition (range 30/120), Light, Loading',
+      magical: false
+    },
+    {
+      name: 'Heavy Crossbow',
+      type: ItemCompendium.Filter.MARTIAL_RANGED_WEAPON,
+      cost: '50 gp',
+      damage: '1d10 Piercing',
+      weight: '18 lb.',
+      properties: 'Ammunition (range 100/400), Heavy, Loading, Two-Handed',
+      magical: false
+    },
+    {
+      name: 'Javelin',
+      type: ItemCompendium.Filter.SIMPLE_MELEE_WEAPON,
+      cost: '5 sp',
+      damage: '1d6 Piercing',
+      weight: '2 lb.',
+      properties: 'Thrown (range 30/120)',
+      magical: false
+    },
+    {
+      name: 'Lance',
+      type: ItemCompendium.Filter.MARTIAL_MELEE_WEAPON,
+      cost: '10 gp',
+      damage: '1d12 Piercing',
+      weight: '6 lb.',
+      properties: 'Reach, Special',
+      magical: false
+    },
+    {
+      name: 'Light Crossbow',
+      type: ItemCompendium.Filter.SIMPLE_RANGED_WEAPON,
+      cost: '25 gp',
+      damage: '1d8 piercing',
+      weight: '5 lb.',
+      properties: 'Ammunition (range 80/320), Loading, Two-Handed',
+      magical: false
+    },
+    {
+      name: 'Light Hammer',
+      type: ItemCompendium.Filter.SIMPLE_MELEE_WEAPON,
+      cost: '2 gp',
+      damage: '1d4 Bludgeoning',
+      weight: '2 lb.',
+      properties: 'Light, Thrown (range 20/60)',
+      magical: false
+    },
+    {
+      name: 'Longbow',
+      type: ItemCompendium.Filter.MARTIAL_RANGED_WEAPON,
+      cost: '50 gp',
+      damage: '1d8 Piercing',
+      weight: '2 lb.',
+      properties: 'Ammunition (range 150/600), Heavy, Two-Handed',
+      magical: false
+    },
+    {
+      name: 'Longsword',
+      type: ItemCompendium.Filter.MARTIAL_MELEE_WEAPON,
+      cost: '15 gp',
+      damage: '1d8 Slashing',
+      weight: '3 lb.',
+      properties: 'Versatile (1d10)',
+      magical: false
+    },
+    {
+      name: 'Mace',
+      type: ItemCompendium.Filter.SIMPLE_MELEE_WEAPON,
+      cost: '5 gp',
+      damage: '1d6 Bludgeoning',
+      weight: '4 lb.',
+      properties: null,
+      magical: false
+    },
+    {
+      name: 'Maul',
+      type: ItemCompendium.Filter.MARTIAL_MELEE_WEAPON,
+      cost: '10 gp',
+      damage: '2d6 Bludgeoning',
+      weight: '10 lb.',
+      properties: 'Heavy, Two-Handed',
+      magical: false
+    },
+    {
+      name: 'Morningstar',
+      type: ItemCompendium.Filter.MARTIAL_MELEE_WEAPON,
+      cost: '15 gp',
+      damage: '1d8 Piercing',
+      weight: '4 lb.',
+      properties: null,
+      magical: false
+    },
+    {
+      name: 'Net',
+      type: ItemCompendium.Filter.MARTIAL_RANGED_WEAPON,
+      cost: '1 gp',
+      damage: null,
+      weight: '3 lb.',
+      properties: 'Special, Thrown (range 5/15)',
+      magical: false
+    },
+    {
+      name: 'Pike',
+      type: ItemCompendium.Filter.MARTIAL_MELEE_WEAPON,
+      cost: '5 gp',
+      damage: '1d10 Piercing',
+      weight: '18 lb.',
+      properties: 'Heavy, Reach, Two-Handed',
+      magical: false
+    },
+    {
+      name: 'Quarterstaff',
+      type: ItemCompendium.Filter.SIMPLE_MELEE_WEAPON,
+      cost: '2 sp',
+      damage: '1d6 Bludgeoning',
+      weight: '4 lb.',
+      properties: 'Versatile (1d8)',
+      magical: false
+    },
+    {
+      name: 'Rapier',
+      type: ItemCompendium.Filter.MARTIAL_MELEE_WEAPON,
+      cost: '25 gp',
+      damage: '1d8 Piercing',
+      weight: '2 lb.',
+      properties: 'Finesse',
+      magical: false
+    },
+    {
+      name: 'Scimitar',
+      type: ItemCompendium.Filter.MARTIAL_MELEE_WEAPON,
+      cost: '25 gp',
+      damage: '1d6 Slashing',
+      weight: '3 lb.',
+      properties: 'Finesse, Light',
+      magical: false
+    },
+    {
+      name: 'Shortbow',
+      type: ItemCompendium.Filter.SIMPLE_RANGED_WEAPON,
+      cost: '25 gp',
+      damage: '1d6 piercing',
+      weight: '2 lb.',
+      properties: 'Ammunition (range 80/320), Two-Handed',
+      magical: false
+    },
+    {
+      name: 'Shortsword',
+      type: ItemCompendium.Filter.MARTIAL_MELEE_WEAPON,
+      cost: '10 gp',
+      damage: '1d6 Piercing',
+      weight: '2 lb.',
+      properties: 'Finesse, Light',
+      magical: false
+    },
+    {
+      name: 'Sickle',
+      type: ItemCompendium.Filter.SIMPLE_MELEE_WEAPON,
+      cost: '1 gp',
+      damage: '1d4 Slashing',
+      weight: '2 lb.',
+      properties: 'Light',
+      magical: false
+    },
+    {
+      name: 'Sling',
+      type: ItemCompendium.Filter.SIMPLE_RANGED_WEAPON,
+      cost: '1 sp',
+      damage: '1d4 Bludgeoning',
+      weight: null,
+      properties: 'Ammunition (range 30/120)',
+      magical: false
+    },
+    {
+      name: 'Spear',
+      type: ItemCompendium.Filter.SIMPLE_MELEE_WEAPON,
+      cost: '1 gp',
+      damage: '1d6 Piercing',
+      weight: '3 lb.',
+      properties: 'Thrown (range 20/60), Versatile(1d8)',
+      magical: false
+    },
+    {
+      name: 'Trident',
+      type: ItemCompendium.Filter.MARTIAL_MELEE_WEAPON,
+      cost: '5 gp',
+      damage: '1d6 Piercing',
+      weight: '4 lb.',
+      properties: 'Thrown (range 20/60), Versatile (1d8)',
+      magical: false
+    },
+    {
+      name: 'War Pick',
+      type: ItemCompendium.Filter.MARTIAL_MELEE_WEAPON,
+      cost: '5 gp',
+      damage: '1d8 Piercing',
+      weight: '2 lb.',
+      properties: null,
+      magical: false
+    },
+    {
+      name: 'Warhammer',
+      type: ItemCompendium.Filter.MARTIAL_MELEE_WEAPON,
+      cost: '15 gp',
+      damage: '1d8 Bludgeoning',
+      weight: '2 lb.',
+      properties: 'Versatile (1d10)',
+      magical: false
+    },
+    {
+      name: 'Whip',
+      type: ItemCompendium.Filter.MARTIAL_MELEE_WEAPON,
+      cost: '2 gp',
+      damage: '1d4 Slashing',
+      weight: '3 lb.',
+      properties: 'Finesse, Reach',
+      magical: false
+    },
   ]
 }
 
